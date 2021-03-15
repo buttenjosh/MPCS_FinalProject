411b8f88acb5c740494625f091a48db361ebb6ae
Josh Buttenhoff <32621211+buttenjosh@users.noreply.github.com>
2021-03-15 18:10:40 -0500

    commit

--- todo.py
+++ todo.py
@@ -1,21 +1,11 @@
 # Final Project
 
+import sys
+
 class Task:
     """Representation of task attributes"""
 
-    def __init__(self, created, completed, name, unique_id, priority, due_date, optional):
-        self.created = created
-        self.completed = completed
-        self.name = name
-        self.unique_id = unique_id
-        self.priority = priority
-        self.due_date = due_date
-        self.optional = optional
-    
-class Tasks:
-    """Task list"""
-
-    def __init__(self, created, completed, name, unique_id, priority, due_date, optional, age, match_term, tasks_lists):
+    def __init__(self, created, completed, name, unique_id, priority, due_date, optional, age, match_term):
         self.created = created
         self.completed = completed
         self.name = name
@@ -25,15 +15,20 @@ class Tasks:
         self.optional = optional
         self.age = age
         self.match_term = match_term
-        self.tasks_lists = []
+    
+class Tasks:
+    """Task list"""
+    def __init__(self):
+        self.tasks_list = []
 
-    def add_task(self, name, due_date, priority):
+    def add(self):
         """Prompts to add task"""
-        self.name = name 
-        self.due_date = due_date
-        self.priority = priority
+              
+        new_task = Task(name = input("Task name: "), due_date = input("Due date: "), priority = input("Priority: "))
+	    self.tasks_list.append(new_task)
+		print(“Added a new task”, len(self.tasks_list), new_task.name)
         
-    def list_task(self, unique_id, age, due_date, priority, name):
+    def list_task(self):
         """Displays a list of the uncompleted tasks sorted by the due date"""
         self.unique_id = unique_id
         self.age = age
@@ -41,7 +36,7 @@ class Tasks:
         self.priority = priority
         self.name = name
     
-    def list_task_search(self, match_term, unique_id, age, due_date, priority, name):
+    def list_task_search(self):
         """Search for tasks that match a term"""
         self.match_term = match_term
         self.unique_id = unique_id
@@ -50,15 +45,15 @@ class Tasks:
         self.priority = priority
         self.name = name        
         
-    def done(self, unique_id):
+    def done(self):
         """Complestes a task"""
         self.unique_id = unique_id
 
-    def delete(self, unique_id):
+    def delete(self):
         """Deletes a task by passing the delete command and the unique identifier"""
         self.unique_id = unique_id
 
-    def report(self, unique_id, age, due_date, priority, name, created, completed):
+    def report(self):
         """Lists all tasks, including both completed and incomplete tasks"""
         self.unique_id = unique_id               
         self.age = age
@@ -67,8 +62,9 @@ class Tasks:
         self.name = name
         self.created = created
         self.completed = completed
-        
-        
 
-
-       
+def do_action():
+	if “add” == sys.argv[1]:
+        add()  
+        
+do_action(tasks, sys.argv[1:])     
